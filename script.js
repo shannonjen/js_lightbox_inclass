95e317b3fad7b620d99e78a39fae0f1f1cafe572
Jennifer Shannon <jen.shannon@gmail.com>
2015-12-11 14:58:56 -0500

    Remove variable

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(document).ready(function(){
 		
 		$(".lightbox-link").on("click",function(e){
 			e.preventDefault();
-			var $image = $("#chosen-image");
-			$image.attr("src", $(this).attr("src"));
+			$("#chosen-image").attr("src", $(this).attr("src"));
 			$(".outer-lightbox").show();
 			$(".outer-lightbox").on('click', function(){
 				$(".outer-lightbox").hide();
